8c9f20c80cd0ba6de61db11bcf3f004c92175600
Nakibuuka Juliet <julietandrewz@gmail.com>
2022-08-01 13:02:51 +0300

    using models

--- code/Resources/item.py
+++ code/Resources/item.py
@@ -1,8 +1,8 @@
-from multiprocessing import connection
-from os import name
+import json
 import sqlite3
 from flask_restful import Resource, reqparse
 from flask_jwt import jwt_required
+from Models.item_model import itemModel
 
 class Item(Resource):
     parser=reqparse.RequestParser()
@@ -13,39 +13,18 @@ class Item(Resource):
 
     @jwt_required()
     def get(self, name):
-        item =self.find_by_name(name)
+        item =itemModel.find_by_name(name)
         if item:
-         return item
+         return item.json()
         return{"message":"Item not found"}, 404
-    
-    @classmethod   
-    def find_by_name(cls,name):
-        connection =sqlite3.connect('data.db')
-        cursor=connection.cursor()
-
-        query="SELECT * FROM items WHERE name=?" 
-        result=cursor.execute(query,(name,))
-        row=result.fetchone()
-        connection.close()
-
-        if row:
-            return {"item":{"name":row[0], "price":row[1]}}
 
     def post(self, name):
-        if self.find_by_name(name) : 
+        if itemModel.find_by_name(name) : 
             return {'messsage':"An item with name'{}' already exists.".format(name)},400
 
         data=Item.parser.parse_args()
-        item= {'name': name, 'price': data['price']}
-
-        connection=sqlite3.connect('data.db')
-        cursor=connection.cursor()
-
-        query="INSERT INTO items VALUES(?, ?)"
-        cursor.execute(query, (item['name'], item['price']))
-
-        connection.commit() 
-        connection.close()
+        item= itemModel(name, data['price'])
+        item.insert()
         return item, 201
 
     def delete(self,name):
@@ -62,24 +41,17 @@ class Item(Resource):
     def put(self, name):
     
         data=Item.parser.parse_args()
-        item=self.find_by_name(name)
-        updated_item= {'name': name, 'price': data['price']}
+        item=itemModel.find_by_name(name)
+        updated_item= itemModel(name, data['price'])
 
         if item is None:
-            self.insert(updated_item)
+            # self.insert(updated_item)
+            updated_item.insert()
         else:
-          item.update(updated_item)
-        return updated_item
+        #   item.update(updated_item)
+            updated_item.update()
+        return updated_item.json()
     
-    @classmethod
-    def update(cls,item):
-        connection =sqlite3.connect('data.db')
-        cursor=connection.cursor()
-
-        query="UPDATE items SET price=? WHERE name=?" 
-        result=cursor.execute(query, (item['name'], item['price']))
-        row=result.fetchone()
-        connection.close()
 
 class ItemList(Resource):
     @jwt_required()
